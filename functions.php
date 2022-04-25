dcb5d6451bdc83317b55a6e08c15e28d37df8117
Ahmed Redwan <aredwan@data-path.net>
2022-04-25 12:48:07 +0600

    Complete functions module

---
+++ functions.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Functions</h1>
+    <?php 
+    
+        /*Defining a Function */
+
+        function writeMessage(){
+            echo "You are a really nice person <br />";
+        }
+        //Calling a function
+        writeMessage();
+        
+        echo "<hr>";
+        
+        writeMessage();
+
+        //Functions with parameters
+
+        function addFunction($num1, $num2){
+            $sum = $num1 + $num2;
+            echo "The sum of $num1 and $num2 is: $sum <br />";
+        }
+        function changeNum(&$num){
+            $num = $num + 10;
+        }
+        $num = 500;
+        addFunction(10,20);
+
+        changeNum($num);
+        echo $num . '<br />';
+    ?>
+        
+
+</body>
+</html>
